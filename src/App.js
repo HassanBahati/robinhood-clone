d83cd7f61d1b177391601c9b8f6feb159c233a11
HassanBahati <mukisabahati@gmail.com>
2021-10-11 14:02:36 +0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React,{Fragment} from 'react'
+import Header from './components/Header';
 
 function App() {
   return (
    <Fragment>
 {/* HEADER */}
 <div class="app__header">
-
+<Header/>
 </div>
 {/* BODY */}
 <div class="app__body">
