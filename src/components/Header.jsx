8c0c3908942aaad063f398da01a89bb7846bc74c
HassanBahati <mukisabahati@gmail.com>
2021-10-11 12:52:50 +0300

    sexctioned header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
-            
+        <div className="header__wrapper">
+            {/* logo */}
+            <div className="header__logo">
+
+            </div>
+            {/* search */}
+            {/* menuitems */}
         </div>
     )
 }
